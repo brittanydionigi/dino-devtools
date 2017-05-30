05c1b2775b9d2fe10687d6693098a7714f90ee7d
Brittany Storoz <brittanystoroz@gmail.com>
2017-05-30 07:12:27 -0600

    Update scripts

--- public/main.js
+++ public/main.js
@@ -1,28 +1,25 @@
-fetch('http://localhost:3000/api/v1/news')
-  .then(response => response.json())
-  .then(newsArticles => {
-    appendNews(newsArticles);
-  })
-  .catch(error => {
-    displayNotification('There was a problem fetching articles');
-  });
-
-
-const displayNotification = (message) => {
-  $('#notification-box').text(message);
-};
+const articles = [
+  { headline: 'Penguins defeat Senators in 2OT of Game 7, return to Cup Final', byline: 'Bob Loblaw' },
+  { headline: 'Penguins will play Predators in Stanley Cup Final', byline: 'Bob Loblaw' },
+  { headline: 'Playoff Buzz: What we learned Thursday', byline: 'Bob Loblaw' },
+];
 
 const appendNews = (articles) => {
   let articlesFrag = document.createDocumentFragment();
 
   articles.forEach(article => {
     let headline = document.createElement('li');
-    headline.innerText = article.headline;
+    let byline = document.createElement('span');
+    byline.innerText = article.byline;
+    headline.innerHTML = article.headline;
+    headline.appendChild(byline);
     articlesFrag.appendChild(headline);
   });
 
   $('#latest-headlines').append(articlesFrag);
 };
+
+appendNews(articles);
 
 
 if ('serviceWorker' in navigator) {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', event => {
   console.log('Service worker installed!');
   event.waitUntil(
-    caches.open('assets-v1').then(cache => {
+    caches.open('assets-v2').then(cache => {
       return cache.addAll([
         './index.html',
         './css/main.css',
@@ -9,6 +9,20 @@ self.addEventListener('install', event => {
         './main.js',
         './img/logo.png'
       ])
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['assets-v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
