5d724353ad831c051e0dccea30ec467fdf77436a
Brittany Storoz <brittanystoroz@gmail.com>
2017-05-31 00:00:47 -0600

    Switch to dexie

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,112 +1,25 @@
+import Dexie from 'dexie';
 import { appendArticles } from './main.js';
 
-// IndexedDB Variables
-const DB_NAME = 'offlineArticles';
-const DB_VERSION = 2;
-const DB_STORE_NAME = 'articles';
+let db = new Dexie('fakeNews');
 
-let db;
-
-// Set up our IndexedDB Database
-function setupIndexedDB() {
-  let req = indexedDB.open(DB_NAME, DB_VERSION);
-
-  req.onsuccess = function(event) {
-    console.log("IndexedDB opened successfully");
-    db = this.result;
-  };
-
-  req.onerror = function(event) {
-    console.error("DB could not open: ", event);
-  };
-
-  req.onupgradeneeded = function(event) {
-    let dataB = event.target.result;
-
-    if (dataB.objectStoreNames.contains(DB_STORE_NAME)) {
-      dataB.deleteObjectStore(DB_STORE_NAME);
-    }
-
-    let store = dataB.createObjectStore(
-      DB_STORE_NAME, { keyPath: 'id' }
-    );
-
-    store.createIndex('headline', 'headline', { unique: false });
-    store.createIndex('byline', 'byline', { unique: false });
-    store.createIndex('id', 'id', { unique: true });
-  };
-};
-
-// Cycle through IndexedDB Records and log values
-export const loadOfflineArticles = () => {
-  console.log('Logging DB Records!');
-  let objectStore = db.transaction(DB_STORE_NAME, 'readwrite').objectStore(DB_STORE_NAME);
-  let headlineIndex = objectStore.index('headline');
-  let articles = [];
-
-  objectStore.openCursor().onsuccess = (event) => {
-    console.log("Opened cursor...");
-    let cursor = event.target.result;
-    if (cursor) {
-      console.log('cursor: ', cursor.value);
-      articles.push(cursor.value);
-      cursor.continue();
-    } else {
-      console.log('appending articles: ', articles);
-      appendArticles(articles);
-    }
-  }
-}
-
-export const checkOfflineArticle = (articleId) => {
-  let dbReq = indexedDB.open('offlineArticles');
-
-  dbReq.onsuccess = (event) => {
-    console.log('DB opened from service worker');
-    
-    let db = event.target.result;
-    let transaction = db.transaction(['articles'], 'readwrite');
-
-    let objectStore = transaction.objectStore("articles");
-    let objectStoreReq = objectStore.get(articleId);
-
-    objectStoreReq.onsuccess = (event) => {
-      console.log('it exists!');
-      return true;
-    }
-
-    objectStoreReq.onerror = (event) => {
-      return false;
-    }
-  };
-}
+db.version(1).stores({
+  articles: 'id, headline, byline'
+});
 
 export const saveForOfflineReading = (article) => {
-  console.log("ARTICLE: ", article);
-  let dbReq = indexedDB.open('offlineArticles');
-
-  dbReq.onsuccess = (event) => {
-    console.log('DB opened from service worker');
-    
-    let db = event.target.result;
-    let transaction = db.transaction(['articles'], 'readwrite');
-
-    // transaction.oncomplete = (event) => {
-    //   console.log('Transaction Success');
-    // }
-
-    // transaction.onerror = (event) => {
-    //   console.log('Transaction Error');
-    // }
-
-    let objectStore = transaction.objectStore("articles");
-    let objectStoreReq = objectStore.add(article);
-
-    objectStoreReq.onsuccess = (event) => {
-      console.log('objectStore request succeeded');    
-      $(`li#article-${article.id}`).addClass('starred');
-    }
-  };
+  db.articles.put(article).then(id => {
+    console.log('id: ', id); 
+    $(`li#article-${id}`).addClass('starred');
+  })
 }
 
-setupIndexedDB();
+export const loadOfflineArticles = () => {
+  db.articles.toArray()
+  .then(articles => {
+    appendArticles(articles);
+  })
+  .catch(error => {
+    console.log('error: ', error);
+  })
+};
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('assets-v11').then(cache => {
+    caches.open('assets-v13').then(cache => {
       return cache.addAll([
         '/',
         '/css/main.css',
@@ -21,7 +21,7 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('activate', (event) => {
-  let cacheWhitelist = ['assets-v11'];
+  let cacheWhitelist = ['assets-v13'];
 
   event.waitUntil(
     caches.keys().then(keyList => {
