97f1d6a2918a1edcdfecc64ea2d877b504999a90
Brittany Storoz <brittanystoroz@gmail.com>
2017-06-15 11:15:21 -0600

    Ahhh

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,70 +1,70 @@
-let syncQueue = [];
+// let syncQueue = [];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('assets-v7').then(cache => {
-      return cache.addAll([
-        '/',
-        '/css/main.css',
-        '/lib/jquery-3.2.1.js',
-        '/img/logo.png',
-        '/bundle.js',
-      ])
-    })
-  );
-});
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open('assets-v7').then(cache => {
+//       return cache.addAll([
+//         '/',
+//         '/css/main.css',
+//         '/lib/jquery-3.2.1.js',
+//         '/img/logo.png',
+//         '/bundle.js',
+//       ])
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
 
-self.addEventListener('activate', (event) => {
-  let cacheWhitelist = ['assets-v7'];
+// self.addEventListener('activate', (event) => {
+//   let cacheWhitelist = ['assets-v7'];
 
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
-    }).then(() => clients.claim())
-  );
-});
+//   event.waitUntil(
+//     caches.keys().then(keyList => {
+//       return Promise.all(keyList.map(key => {
+//         if (cacheWhitelist.indexOf(key) === -1) {
+//           return caches.delete(key);
+//         }
+//       }));
+//     }).then(() => clients.claim())
+//   );
+// });
 
-const addArticle = () => {
-  let newestArticle = syncQueue.shift();
-  return fetch('/api/v1/articles', {
-    method: 'POST',
-    body: JSON.stringify(newestArticle),
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
+// const addArticle = () => {
+//   let newestArticle = syncQueue.shift();
+//   return fetch('/api/v1/articles', {
+//     method: 'POST',
+//     body: JSON.stringify(newestArticle),
+//     headers: { 'Content-Type': 'application/json' }
+//   });
+// }
 
-self.addEventListener('message', (event) => {
-  if (event.data.type === 'add-article') {
-    syncQueue.push(event.data.article);
-  }
-  self.registration.sync.register('addArticle')
-});
+// self.addEventListener('message', (event) => {
+//   if (event.data.type === 'add-article') {
+//     syncQueue.push(event.data.article);
+//   }
+//   self.registration.sync.register('addArticle')
+// });
 
-self.addEventListener('sync', function (event) {
-  if (event.tag === 'addArticle') {
-    event.waitUntil(addArticle()
-      .then(response => response.json())
-      .then(articles => {
-        self.clients.matchAll().then(clients => {
-          clients[0].postMessage({ articles: articles });
-        });
-        self.registration.showNotification("Added new article");
-      })
-      .catch(error => {
-        console.log('error: ', error);
-      })
-    );
-  }
-});
+// self.addEventListener('sync', function (event) {
+//   if (event.tag === 'addArticle') {
+//     event.waitUntil(addArticle()
+//       .then(response => response.json())
+//       .then(articles => {
+//         self.clients.matchAll().then(clients => {
+//           clients[0].postMessage({ articles: articles });
+//         });
+//         self.registration.showNotification("Added new article");
+//       })
+//       .catch(error => {
+//         console.log('error: ', error);
+//       })
+//     );
+//   }
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,46 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: './public/main.js',
+  devtool: 'source-map',
+  entry: ['./main.scss', './public/js/main.js', './public/assets/img/star.png'],
   output: {
     path: __dirname + '/public',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  }
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property 
+        loader:"file-loader",
+        query:{
+          name:'[name].[ext]',
+          outputPath:'../img/'
+          //the images will be emmited to public/assets/images/ folder 
+          //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png); 
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,        
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: [
+            { loader: 'css-loader?sourceMap', options: { sourceMap: true }},
+            { loader: 'sass-loader?sourceMap', options: { sourceMap: true }}
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+      new ExtractTextPlugin({ 
+        filename: 'assets/css/main.css',
+        allChunks: true
+      })
+    ]
 };
